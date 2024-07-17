69cdbc31f32561595cea76389b4380a51718301d
sevakul16 <sevakul16@gmail.com>
2024-07-17 16:02:23 +0300

    Added player naming

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import Rect, { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={(e) => {
+              setPlayerName(e.target.value);
+            }}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => setIsEditing(!isEditing)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+};
+
+export default Player;
